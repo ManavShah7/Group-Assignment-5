a2bc5378d8e0f53a9ecf4a10cfead25aa2b4eb59
krishi <parekh.krishi@northeastern.edu>
2025-10-09 17:51:01 -0400

    logout function

--- script.js
+++ script.js
@@ -38,3 +38,12 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+    // LOGOUT
+  if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => {
+      localStorage.removeItem('loggedInUser');
+      window.location.href = "login.html";
+    });
+  }
+});
+
